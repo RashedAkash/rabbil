828bd9316e73ab34e053b07fc6530fde2a246bc6
hablu <131282586+hablu@users.noreply.github.com>
2024-02-08 08:16:50 +0600

    error page create

---
+++ src/app/product/error.js
@@ -0,0 +1,11 @@
+"use client";
+
+const error = () => {
+  return (
+    <div>
+      <h1 className=" font-bold text-5xl text-red]">Something went wrong</h1>
+    </div>
+  );
+};
+
+export default error;
