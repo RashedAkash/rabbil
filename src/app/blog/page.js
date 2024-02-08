2f076d622b4b13dce38344d1a92148eeea34f4fc
hablu <131282586+hablu@users.noreply.github.com>
2024-02-08 07:46:15 +0600

    many page create

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      
+      <h1>This is Blog page</h1>
     </div>
   );
 };
--- src/app/product/page.js
+++ src/app/product/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      
+      <h1>This is product page</h1>
     </div>
   );
 };
