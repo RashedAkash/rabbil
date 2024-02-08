ddf14b8c5239ffec1f6c86d5a5d84004b5dd4e75
hablu <131282586+hablu@users.noreply.github.com>
2024-02-08 08:18:51 +0600

    loading page create

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const loading = () => {
+  return (
+    <div>
+      <h1 className=" font-bold text-5xl text-red]">Loading......</h1>
+    </div>
+  );
+};
+
+export default loading;
