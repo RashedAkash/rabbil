0d143dff30095d099882ed1825a4468abb8f5722
hablu <131282586+hablu@users.noreply.github.com>
2024-02-11 07:59:01 +0600

    multiple data added

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,24 @@
-import React from 'react';
+const getData = async () => {
+  try {
+    const res1 = await fetch("https://jsonplaceholder.typicode.com/users")
+   
+    const res2 = await fetch("https://dummyjson.com/products");
+    const data1 = await res1.json()
+    const data2 = await res2.json()
 
-const page = () => {
+    return {data1:data1,data2:data2}
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const page = async() => {
+  const data = await getData()
+  console.log(data);
   return (
     <div>
       <h1>This is Dashboard page</h1>
+      
     </div>
   );
 };
